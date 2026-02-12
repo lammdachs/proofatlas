780a824afdd0a80decd56caa05824e69f672b2d1
Alexander Pluska <alexander.pluska@tuwien.ac.at>
2026-02-12 13:28:55 +0100

    deleted debug scripts

--- scripts/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Test script for proofatlas - sets up libtorch environment and runs cargo test
-
-export LIBTORCH_USE_PYTORCH=1
-export LD_LIBRARY_PATH=$(python3 -c "import torch; print(torch.__path__[0])")/lib:$LD_LIBRARY_PATH
-
-cargo test "$@"
